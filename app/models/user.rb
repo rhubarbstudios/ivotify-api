50dc24d6de38da80803f83251a143d256408ca4d
7sun <pakrouse@gmail.com>
2015-10-02 16:43:32 -0700

    fixes validation typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
-  validates :password, presence_confirmation: true#, on: :create
+  validates :password_confirmation, presence: true#, on: :create
 
   def is_admin?
     self.admin == true
