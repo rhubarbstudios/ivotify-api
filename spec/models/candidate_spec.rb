5d108c1a11cc6108f14176d8e376674284aa8f01
7sun <pakrouse@gmail.com>
2015-10-06 17:33:53 -0700

    adds candidate model tests

--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Candidate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe Candidate do
+
+    it "is valid with a first name, last name and bio" do
+      candidate = build(:candidate)
+      expect(candidate).to be_valid
+    end
+
+    it "is invalid without a first_name" do
+      candidate = build(:candidate, first_name: nil)
+      expect(candidate).to be_invalid
+    end
+
+    it "is invalid without a last_name" do
+      candidate = build(:candidate, last_name: nil)
+      expect(candidate).to be_invalid
+    end
+
+    it "is invalid without a bio" do
+      candidate = build(:candidate, bio: nil)
+      expect(candidate).to be_invalid
+    end
+
+    it "returns a contact's full name as a string" do
+      candidate = build(:candidate, first_name: "George", last_name: "Washington")
+      expect(candidate.full_name).to eq "George Washington"
+    end
+
+  end
+
 end
