beed9a86c5483caf85d78015a4480956b93f9c85
Alex Dinari <alexdinari@roadhouseapps.com>
2015-10-30 15:51:08 -0700

    added dependent destory for issues with their quotes

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,5 @@
 class Issue < ActiveRecord::Base
-  has_many :quotes
+  has_many :quotes, dependent: :destroy
 
   validates :title, presence: true, uniqueness: true
   validates :summary, presence: true
