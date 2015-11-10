8678b003f71940b6ea1f0089614baee606f6dcda
Alex Dinari <alexdinari@roadhouseapps.com>
2015-11-10 15:06:47 -0800

    checking to see if the build works

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'application#index'
 
+# Fixed Routes
   namespace :api, defaults: { format: "json" } do
     resources :issues, :candidates, :users, :feedbacks
     resources :sessions, only: [:create, :destroy]
