351158b07e7ddc054d6bce716faf75e4dcae6628
Alex Dinari <alexdinari@roadhouseapps.com>
2015-12-01 16:50:41 -0800

    added the array of bullets to issue_side

--- db/migrate/20151201235202_add_column_to_issue_side.rb
+++ db/migrate/20151201235202_add_column_to_issue_side.rb
@@ -1,5 +1,5 @@
 class AddColumnToIssueSide < ActiveRecord::Migration
   def change
-    add_column :bullets, :string, array: true, default: '{}'
+    add_column :issue_sides, :bullets, :string, array: true, default: '{}'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117002501) do
+ActiveRecord::Schema.define(version: 20151201235202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +33,9 @@ ActiveRecord::Schema.define(version: 20151117002501) do
   create_table "issue_sides", force: :cascade do |t|
     t.string   "title"
     t.integer  "issue_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.string   "bullets",    default: [],              array: true
   end
 
   add_index "issue_sides", ["issue_id"], name: "index_issue_sides_on_issue_id", using: :btree
